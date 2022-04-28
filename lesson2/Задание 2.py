e25db7fb2077a3c1005d3ff8050155df20da6526
Alens-star <57572369+Alens-star@users.noreply.github.com>
2022-04-28 11:31:02 +0300

    Update Задание 2.py

--- lesson2/Задание 2.py
+++ lesson2/Задание 2.py
@@ -6,10 +6,13 @@ if len(string) > 1 and len(string) % 2 == 0:
     while i != len(string):
         list_symbols[i+1], list_symbols[i] = list_symbols[i], list_symbols[i+1]
         i += 2
+    result_string = ''.join(list_symbols)
 elif len(string) > 1 and len(string) % 2 != 0:
     while i != len(string) - 1:
         list_symbols[i + 1], list_symbols[i] = list_symbols[i], list_symbols[i + 1]
         i += 2
+    result_string = ''.join(list_symbols)
 else:
     print("Необходимо ввести хотя бы 2 символа")
-print(list_symbols)
+print(result_string)
+
