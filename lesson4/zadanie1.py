12224884806da0530ea371ff9fecaad34b3e245f
Alens-star <57572369+Alens-star@users.noreply.github.com>
2022-05-08 13:46:36 +0300

    Update zadanie1.py

--- lesson4/zadanie1.py
+++ lesson4/zadanie1.py
@@ -1,4 +1,5 @@
 import sys
 from lesson4 import my_functions
 rubs_list = sys.argv
-print(my_functions.salary_func(rubs_list[1], rubs_list[2]))
+#print(rubs_list[1])
+print(my_functions.salary_func(rubs_list[1], rubs_list[2], rubs_list[3]))
